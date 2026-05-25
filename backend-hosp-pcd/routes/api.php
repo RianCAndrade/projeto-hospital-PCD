8809a8054122b3992824a7ddc720ceee8b7e529e
Rian Andrade <140345488+RianCAndrade@users.noreply.github.com>
2026-05-25 18:51:25 -0300

    algumas rotas

--- backend-hosp-pcd/routes/api.php
+++ backend-hosp-pcd/routes/api.php
@@ -15,11 +15,35 @@ Route::post('/login', [LoginController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function (){
     Route::post('/logout', [LogoutController::class, 'logout']);
 
-    // Rotas usuario
+    // Rotas usuario paciente
     Route::prefix('me')->group(function (){
         Route::get('/', [PerfilController::class, 'index']);
         Route::put('/update/{id}', [PerfilController::class, 'update']);
         Route::delete('/delete/{id}', [PerfilController::class, 'destroy']);
     });
+
+    // Rota de admin
+    Route::prefix('admin')->group(function (){
+
+    });
+
+    // Rota de RH (Hospital)
+    Route::prefix('rh')->group(function (){
+
+    });
+
+    // Rotas Recepcionista
+    Route::prefix('recepcionista')->group(function (){
+
+    });
+
+    // Rota de atendimento
+    Route::prefix('atendimento')->group(function (){
+
+    });
+
+    // Rotas agendamento
     
+
+
 });
